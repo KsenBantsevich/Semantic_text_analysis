644dfc0bb694842e94a188175a6056a619640306
KsenyaBantsevich <ksusha.bantsevich@gmail.com>
2021-03-18 17:21:10 +0300

    Added help menu

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import filedialog as fd
 
+from help import help_text
+import time
+
 
 def open_file():
     file_name = fd.askopenfilename(filetypes=(("TXT files", "*.txt"),))
@@ -8,6 +11,19 @@ def open_file():
         file = open(file_name, 'r')
         data = file.readlines()
         calculated_text.insert(1.0, data)
+
+
+def show_information():
+    children = Toplevel()
+    children.title('Help')
+    children.geometry("600x300+500+350")
+    output_help_text = Text(children, height=20, width=80)
+    scroll_b = Scrollbar(children, command=output_help_text.yview)
+    scroll_b.grid(row=4, column=8, sticky='nsew')
+    output_help_text.grid(row=4, column=0, sticky='nsew', columnspan=3)
+    output_help_text.configure(yscrollcommand=scroll_b.set)
+    output_help_text.insert('end', help_text)
+    output_help_text.configure(state='disabled')
 
 
 root = Tk()
@@ -22,7 +38,7 @@ label.grid(row=0, column=0)
 calculated_text = Text(root, height=5, width=50)
 calculated_text.grid(row=1, column=1, sticky='nsew', columnspan=2)
 
-help_button = Button(text="Help", width=10)
+help_button = Button(text="Help", width=10, command=show_information)
 help_button.grid(row=0, column=3)
 
 open_button = Button(text="Open file", width=10, command=open_file)
